a09e0a7156effe20e2c191f4c941e20b14744df3
matheusnop <matheus_natanaelop@hotmail.com>
2017-11-08 23:52:25 -0200

    second

--- Modo2.c
+++ Modo2.c
@@ -10,16 +10,18 @@ void inicializaTabuleiro(int tabuleiro[][9]){
 void mostraTabuleiro(int tabuleiro[][9], int a, int nivel){
         int linha, coluna;
         if(a==1 && nivel==1){
-            for(coluna=1;coluna<=9;coluna++){
+        for(coluna=0;coluna<9;coluna++){
                 printf("\t%d",coluna);
-            }
+        }
             printf("\n");
             tabuleiro[0][1]=9;
+            tabuleiro[1][5]=7;
+            tabuleiro[8][8]=7;
             for(linha=0 ; linha < 9 ; linha++ ){
                 printf("%c",linha+65);
                 for(coluna=0 ; coluna < 9 ; coluna++ ){
                     if(tabuleiro[linha][coluna]==-1){
-                        printf(" ");
+                        printf("\t%d",tabuleiro[linha][coluna]);
                     }
                     else{
                         printf("\t%d",tabuleiro[linha][coluna]);
@@ -28,120 +30,7 @@ void mostraTabuleiro(int tabuleiro[][9], int a, int nivel){
                 printf("\n\n");
             }
         }
-        else if(a==2 && nivel==1){
-            for(coluna=1;coluna<=9;coluna++){
-                printf("\t%d",coluna);
-            }
-            printf("\n");
-            tabuleiro[0][1]=2;
-            for(linha=0 ; linha < 9 ; linha++ ){
-                printf("%c",linha+65);
-                for(coluna=0 ; coluna < 9 ; coluna++ ){
-                    if(tabuleiro[linha][coluna]==-1){
-                        printf(" ");
-                    }
-                    else{
-                        printf("\t%d",tabuleiro[linha][coluna]);
-                    }
-                }
-                printf("\n\n");
-            }
-        }
-        else if(a==3 && nivel==1){
-            for(coluna=1;coluna<=9;coluna++){
-                printf("\t%d",coluna);
-            }
-            printf("\n");
-            tabuleiro[0][1]=3;
-            for(linha=0 ; linha < 9 ; linha++ ){
-                printf("%c",linha+65);
-                for(coluna=0 ; coluna < 9 ; coluna++ ){
-                    if(tabuleiro[linha][coluna]==-1){
-                        printf(" ");
-                    }
-                    else{
-                        printf("\t%d",tabuleiro[linha][coluna]);
-                    }
-                }
-                printf("\n\n");
-            }
-        }
-        else if(a==4 && nivel==1){
-            for(coluna=1;coluna<=9;coluna++){
-                printf("\t%d",coluna);
-            }
-            printf("\n");
-            tabuleiro[0][1]=4;
-            for(linha=0 ; linha < 9 ; linha++ ){
-                printf("%c",linha+65);
-                for(coluna=0 ; coluna < 9 ; coluna++ ){
-                    if(tabuleiro[linha][coluna]==-1){
-                        printf(" ");
-                    }
-                    else{
-                        printf("\t%d",tabuleiro[linha][coluna]);
-                    }
-                }
-                printf("\n\n");
-            }
-        }
-        else if(a==5 && nivel==1){
-            for(coluna=1;coluna<=9;coluna++){
-                printf("\t%d",coluna);
-            }
-            printf("\n");
-            tabuleiro[0][1]=5;
-            for(linha=0 ; linha < 9 ; linha++ ){
-                printf("%c",linha+65);
-                for(coluna=0 ; coluna < 9 ; coluna++ ){
-                    if(tabuleiro[linha][coluna]==-1){
-                        printf(" ");
-                    }
-                    else{
-                        printf("\t%d",tabuleiro[linha][coluna]);
-                    }
-                }
-                printf("\n\n");
-            }
-        }
-        else if(a==6 && nivel==1){
-            for(coluna=1;coluna<=9;coluna++){
-                printf("\t%d",coluna);
-            }
-            printf("\n");
-            tabuleiro[0][1]=6;
-            for(linha=0 ; linha < 9 ; linha++ ){
-                printf("%c",linha+65);
-                for(coluna=0 ; coluna < 9 ; coluna++ ){
-                    if(tabuleiro[linha][coluna]==-1){
-                        printf(" ");
-                    }
-                    else{
-                        printf("\t%d",tabuleiro[linha][coluna]);
-                    }
-                }
-                printf("\n\n");
-            }
-        }
-        else if(a==7 && nivel==1){
-            for(coluna=1;coluna<=9;coluna++){
-                printf("\t%d",coluna);
-            }
-            printf("\n");
-            tabuleiro[0][1]=7;
-            for(linha=0 ; linha < 9 ; linha++ ){
-                printf("%c",linha+65);
-                for(coluna=0 ; coluna < 9 ; coluna++ ){
-                    if(tabuleiro[linha][coluna]==-1){
-                        printf(" ");
-                    }
-                    else{
-                        printf("\t%d",tabuleiro[linha][coluna]);
-                    }
-                }
-                printf("\n\n");
-            }
-        }
+
 }
 int main(){
     setlocale(LC_ALL, "Portuguese");
@@ -153,13 +42,12 @@ int main(){
     inicializaTabuleiro(tabuleiro);
     switch (nivel){
         case 1:
-            a=rand()%7;
+            a=rand()%2;
             printf("%d",a);
              mostraTabuleiro(tabuleiro,a,nivel);
              break;
         case 2:
              a=rand()%7;
-
              mostraTabuleiro(tabuleiro,a,nivel);
              break;
         case 3:
